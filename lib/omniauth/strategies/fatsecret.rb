2121b0947b01762f2d1eafc8ceb54b7b8ad30633
scrawlon <scrawlon@gmail.com>
2013-07-26 20:55:59 +0000

    Removed unnecessary OAuth options from fatsecret.rb

--- lib/omniauth/strategies/fatsecret.rb
+++ lib/omniauth/strategies/fatsecret.rb
@@ -5,9 +5,6 @@ module OmniAuth
     class Fatsecret < OmniAuth::Strategies::OAuth
       option :client_options, {
         :site               => 'http://www.fatsecret.com',
-        :format             => 'json',
-        :oauth_nonce        => Time.now.to_i.to_s,
-        :oauth_timestamp    => Time.now.to_i,
         :scheme             => :query_string,
         :http_method        => :get
       }
