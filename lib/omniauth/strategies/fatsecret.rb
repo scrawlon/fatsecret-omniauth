835696316a2d43a0ba914284c0ed3d89b285882e
scrawlon <scrawlon@gmail.com>
2013-08-03 16:26:36 +0000

    Added Fatsecret profile data in OmniAuth::AuthHash::InfoHash in fatsecret.rb

--- lib/omniauth/strategies/fatsecret.rb
+++ lib/omniauth/strategies/fatsecret.rb
@@ -1,4 +1,5 @@
 require 'omniauth-oauth'
+require 'multi_json'
 
 module OmniAuth
   module Strategies
@@ -8,6 +9,22 @@ module OmniAuth
         :scheme             => :query_string,
         :http_method        => :get
       }
+
+      info do
+        {
+          goal_weight_kg: raw_info['goal_weight_kg'],
+          height_cm: raw_info['height_cm'],
+          height_measure: raw_info['height_measure'],
+          last_weight_comment: raw_info['last_weight_comment'],
+          last_weight_date_int: raw_info['last_weight_date_int'],
+          last_weight_kg: raw_info['last_weight_kg'],
+          weight_measure: raw_info['weight_measure']
+        }
+      end
+
+      def raw_info
+        @raw_info ||= MultiJson.decode(access_token.get('http://platform.fatsecret.com/rest/server.api?method=profile.get&format=json').body)['profile']
+      end
     end
   end
 end
